888c341a328188ba154e7a543042c12b4d98252f
Jordan Zdimirovic <jordstar20001@gmail.com>
2020-09-16 12:43:39 +1000

    ✖ Remove redundant TCPServer handler.

--- server/socket_helper.py
+++ server/socket_helper.py
@@ -1,13 +1,5 @@
 from socketserver import TCPServer, BaseRequestHandler as brh
 from types import FunctionType
-class TCPFunctionDelegator(brh):
-    callback = None
-    def __init__(self, _callback):
-        assert _callback.__code__.co_argcount == 1, "Callback must only have one arg: the BaseRequestHandler of the request"
-        self.callback = _callback
-
-    def handle(self):
-        self.callback(self)
 
 class ClientSocketManager():
     _TCPServer = None
